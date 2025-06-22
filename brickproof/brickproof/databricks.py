30bc39fe7d08c3a670061dbc1dfc6f705d1ce8b6
Jordan Young <jordanyoung@MacBookPro.attlocal.net>
2025-06-22 15:57:39 -0500

    [feat/more] python code and tests

--- brickproof/brickproof/databricks.py
+++ brickproof/brickproof/databricks.py
@@ -1,5 +1,5 @@
 import requests
-import constants as c
+import brickproof.constants as c
 
 
 class DatabricksHandler:
@@ -11,7 +11,7 @@ class DatabricksHandler:
 
         self.workspace_url = workspace_url
 
-    def list_files(self, workspace_path: str):
+    def list_files(self, workspace_path: str) -> requests.Response:
         url = f"{self.workspace_url}{c.LIST_WORKSPACE_FILES_ENDPOINT}"
         params = {"path": workspace_path}
         response = requests.get(url=url, params=params, headers=self.headers)
@@ -25,8 +25,8 @@ class DatabricksHandler:
 
         return response
 
-    def check_for_git_folder(self, workspace_path: str):
-        #TODO functionality not completely flushed out yet. fix that!
+    def check_for_git_folder(self, workspace_path: str) -> requests.Response:
+        # TODO functionality not completely flushed out yet. fix that!
 
         url = f"{self.url}/{c.GET_REPOS_ENDPOINT}"
         params = {"path": workspace_path}
@@ -34,21 +34,23 @@ class DatabricksHandler:
 
         return response
 
-    def create_git_folder(self, git_payload: dict):
+    def create_git_folder(self, git_payload: dict) -> requests.Response:
         url = f"{self.url}/{c.CREATE_REPO_ENDPOINT}"
-        response = requests.post(url=url,data=git_payload,headers=self.headers)
+        response = requests.post(url=url, data=git_payload, headers=self.headers)
         return response
 
-    def remove_git_folder(self, repo_id: str):
-
+    def remove_git_folder(self, repo_id: str) -> requests.Response:
         url = f"{self.workspace_url}/{c.CREATE_REPO_ENDPOINT}/{repo_id}"
         response = requests.delete(url=url, headers=self.headers)
         return response
 
-    def create_job(self):
-        pass
+    def create_job(self, job_payload: dict) -> requests.Response:
+        url = f"{self.workspace_url}/{c.CREATE_JOB_ENDPOINT}"
+        response = requests.post(url=url, data=job_payload, headers=self.headers)
+        return response
 
-    def run_job(self):
+    def trigger_job(self):
+
         pass
 
     def check_job(self):
@@ -56,3 +58,15 @@ class DatabricksHandler:
 
     def remove_job(self):
         pass
+
+    def upload_runner(self, upload_payload: dict) -> requests.Response:
+        url = f"{self.workspace_url}/{c.UPLOAD_FILE_ENDPOINT}"
+        response = requests.post(url=url, data=upload_payload, headers=self.headers)
+        return response
+
+
+    def one_off_submission(self, payload: dict) -> requests.Response:
+        #Experimental, could be helpful!
+        url = f"{self.workspace_url}/{c.ONE_OFF_SUBMISSION}"
+        response = requests.post(url=url,data=payload,headers=self.headers)
+        return response
--- tests/test_brickproof.py
+++
@@ -1,8 +0,0 @@
-from brickproof.__main__ import main
-
-
-def test_main():
-    real = main()
-    target = 0
-
-    assert real == target
