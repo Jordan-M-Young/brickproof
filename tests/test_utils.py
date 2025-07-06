400fc87f1c3473b3fc40ab6562feec9e1ff38679
Jordan Young <jordanyoung@MacBookPro.attlocal.net>
2025-07-06 18:14:34 -0500

    another one

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -57,9 +57,10 @@ def test_insert_ignore_statement():
         """retcode = pytest.main(["-p", "no:cacheprovider","--capture=sys"{ignore}])"""
     )
     ignore = ["./tests/test_dummy.py"]
-    real = insert_ignore_statement(runner_str, ignore)
+    repo_name = "dummy"
+    real = insert_ignore_statement(runner_str, ignore, repo_name)
 
-    target = """retcode = pytest.main(["-p", "no:cacheprovider","--capture=sys","--ignore=./tests/test_dummy.py"])"""
+    target = """retcode = pytest.main(["-p", "no:cacheprovider","--capture=sys","--ignore=dummy/tests/test_dummy.py"])"""
     assert real == target
 
 
@@ -67,7 +68,7 @@ def test_insert_dependencies():
     requirements = ["requests", "tomlkit"]
     runner_str = "{requirements}"
     real = insert_dependencies(runner_str, requirements)
-    target = "!pip install requests tomlkit"
+    target = "!pip install pytest requests tomlkit"
 
     assert real == target
 
